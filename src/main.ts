7e1a9f39d9f2d105668a4bd8de2357894f7550b8
Petre Leonard Macamete <petreleonardos@gmail.com>
2022-03-08 01:05:02 +0200

    okay?

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log("hey!")
+import express = require( 'express' );
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( _req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
