2a77719332d38e4efa2a903579ce725e5d153aad
Petre Leonard Macamete <petreleonardos@gmail.com>
2022-03-08 00:33:42 +0200

    okay

--- src/main.ts
+++ src/main.ts
@@ -30,3 +30,17 @@ function delayedHello(
 export async function greeter(name: string) {
   return await delayedHello(name, Delays.Long);
 }
+import http from 'http';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
