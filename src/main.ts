f7053470d022cc71e0c76a4de8fc73c182f00bf5
Petre Leonard Macamete <petreleonardos@gmail.com>
2022-03-10 19:43:01 +0200

    hope yes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import express = require( 'express');
+import cors = require('cors');
 const app = express();
 const port = process.env.PORT || 80; // default port to listen
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 // define a route handler for the default home page
 app.get( "/", ( _req, res ) => {
-    res.send( "Hello world!" );
+    res.json( 
+        {
+            message: "Hello World!"
+        }
+    );
 } );
 
 // start the Express server
