5170e6091004d5331a840530ecf259aaa8f830be
Petre Leonard Macamete <petreleonardos@gmail.com>
2022-03-10 19:54:05 +0200

    I am sure it not work :P

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express = require( 'express');
+import { Request, Response } from 'express';
 import cors = require('cors');
 const app = express();
 const port = process.env.PORT || 80; // default port to listen
@@ -7,7 +8,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 // define a route handler for the default home page
-app.get( "/", ( _req, res ) => {
+app.get( "/", ( _req : Request, res :Response) => {
     res.json( 
         {
             message: "Hello World!"
