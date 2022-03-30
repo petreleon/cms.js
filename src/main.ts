0c0e21d7f6417b012e7590941794124ed330a4b2
Petre Leonard Macamete <petreleonardos@gmail.com>
2022-03-31 00:13:07 +0300

    all

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import express = require( 'express');
 import { Request, Response } from 'express';
 import cors = require('cors');
+import fileUpload = require('express-fileupload');
+import login from './authentificator/login';
 const app = express();
 const port = process.env.PORT || 80; // default port to listen
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-
+app.use(fileUpload());
+app.use('/login', login);
 // define a route handler for the default home page
 app.get( "/", ( _req : Request, res :Response) => {
     res.json( 
