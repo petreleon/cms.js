282cfe35ec3a1758680e24ddae844336b3df4698
Petre Leonard Macamete <petreleonardos@gmail.com>
2022-05-03 06:10:44 +0300

    multer aws s3

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,30 @@
 import express = require( 'express');
 import { Request, Response } from 'express';
 import cors = require('cors');
-import fileUpload = require('express-fileupload');
 import login from './authentificator/login';
 import register from './authentificator/register';
 import mongoose from 'mongoose';
 import config from './config/config';
 import verify from './authentificator/verifier';
-
 import { ConnectOptions } from 'mongoose';
+import { routerFiles } from './services/aws/upload/upload';
+
 mongoose.connect(config.mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    } as ConnectOptions, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('Connected to mongoDB');
-        }
+} as ConnectOptions, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to mongoDB');
+    }
 });
-
 
 const app = express();
 const port = process.env.PORT || 80; // default port to listen
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-app.use(fileUpload());
 app.use('/login', login);
 app.use('/register', register);
 // define a route handler for the default home page
@@ -38,6 +36,7 @@ app.get( "/", [verify], ( _req : Request, res :Response) => {
     );
 } );
 
+app.use('/files', routerFiles);
 // start the Express server
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
